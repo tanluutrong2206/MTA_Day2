efeba3d816e58116fbea6bbac7ea82ba9dce210e
Lưu Trọng Tấn <tanluutrong2206@gmail.com>
2021-03-05 11:54:52 +0700

    add hashtable content

--- MTA_Day2/HashTableEx.cs
+++ MTA_Day2/HashTableEx.cs
@@ -16,7 +16,43 @@ namespace MTA_Day2
         public static void Run()
         {
             Hashtable cart = new Hashtable();
-            //cart.Add
+
+            // lay gia tri trong hashtable
+            Console.WriteLine($"Ten cua san pham co id la 1 la: {cart[1]}");
+
+            // check ton tai key, value
+            if (cart.ContainsKey(1) || cart.ContainsValue("Demo"))
+            {
+                Console.WriteLine("Gio hang co ton tai san pham voi id la 1 hoac ten la Demo");
+            } else
+            {
+                Console.WriteLine("Gio hang khong ton tai san pham voi id la 1 hoac ten la Demo");
+            }
+
+            // xoa item theo key
+            cart.Remove(2);
+
+            // in ra hash table
+            foreach (var key in cart.Keys)
+            {
+                Console.WriteLine($"{key} - {cart[key]}");
+            }
+        }
+
+        private static Hashtable Input()
+        {
+            Hashtable cart = new Hashtable();
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Nhap id san pham thu " + i);
+                int id = int.Parse(Console.ReadLine());
+                Console.WriteLine("Nhap ten san pham thu " + i);
+                string name = Console.ReadLine();
+
+                cart.Add(id, name);
+            }
+
+            return cart;
         }
     }
 }
